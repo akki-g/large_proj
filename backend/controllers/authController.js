fe3bab02ccf320b4a234e8a05ec7702ac4a7c4d2
Ubuntu <ubuntu@ip-172-31-80-153.ec2.internal>
2025-03-05 20:54:30 +0000

    api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/Users');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const {v4: uuidv4} = require('uuid');
 
 
