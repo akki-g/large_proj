c66c32eb3ab190017dbed10e5476fc59a882b3a3
akki-g <akshat.guduru@gmail.com>
2025-03-25 16:39:51 -0400

    header issue

--- backend/routes/classRoutes.js
+++ backend/routes/classRoutes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { createClass, searchClass, modifyClass, deleteClass, getAllClasses, getClassWithChapters } = require('../controllers/classController');
+const upload = require('../fileControl/fileUpload');
 
 router.post('/create',  upload.single('syllabus'), createClass);
 router.post('/search', searchClass);
