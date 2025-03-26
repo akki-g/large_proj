885fcc2e8142ba8393b81c1e054350ce5959960b
akki <akshat.guduru@gmail.com>
2025-03-26 17:59:40 -0400

    chat box stuff

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { sendMessage, getChats, getChatById, deleteChat } = require('../controllers/chatController');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json({ limit: '50mb' });
+
+router.post('/send', jsonParser, sendMessage);
+
+router.get('/list', getChats);
+
+router.get('/detail', getChatById);
+
+router.post('/delete', jsonParser, deleteChat);
+
+module.exports = router;
